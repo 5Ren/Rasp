ec083dc8bb718f35ecc90357147864bcd7854260
goren-Y <cocacola@live.jp>
2022-09-28 11:04:58 +0900

    binary変換

---
+++ binary/binary_change.py
@@ -0,0 +1,13 @@
+prog_no = int(25)
+
+print(f'{prog_no=}')
+print(f'DN: {int(prog_no)}')
+print(f'FEX: {hex(prog_no)}')
+print(f'Binary: {bin(prog_no)}\n')
+
+print(f'bi2: {format(prog_no, "b")}')
+print(f'Fex2: {format(prog_no, "x")}')
+
+a = '{:07b}'.format(prog_no)
+print(a)
+print(type(a))
---
+++ binary/bit_binary.py
@@ -0,0 +1,19 @@
+prog_no = int(25)
+
+# prog_no_bi = format(prog_no, 'b')
+# prog_no_bi_1st = format((prog_no // 64), 'b')
+# prog_no_bi_2nd = format(((prog_no % 64) // 32), 'b')
+#
+#
+# print(f'1st: {prog_no_bi_1st}')
+# print(f'2nd: {prog_no_bi_2nd}')
+
+prog_no_bi = '{:07b}'.format(prog_no)
+print(prog_no_bi)
+print(f'1st: {int(prog_no_bi[-1])}')
+print(f'2nd: {int(prog_no_bi[-2])}')
+print(f'3rd: {int(prog_no_bi[-3])}')
+print(f'4th: {int(prog_no_bi[-4])}')
+print(f'5th: {int(prog_no_bi[-5])}')
+print(f'6th: {int(prog_no_bi[-6])}')
+print(f'7th: {int(prog_no_bi[-7])}')
