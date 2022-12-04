552ef39110a5f4abe39ce9c01be4e4553ebcaf47
goren <cocacola@live.jp>
2022-12-04 20:29:42 +0900

    stepping2

--- stepping-m/rotation/rotation.ino
+++ stepping-m/rotation/rotation.ino
@@ -1,12 +1,13 @@
-int cwPin = 10;
-int ccwPin = 11;
-int delayMicroSec = 1000;
+int ssEL = 4;
+int cwPin = 5;
+int ccwPin = 6;
+int delayMicroSec = 16000;
 
 void setup() {
   Serial.begin(19200);
+  pinMode(ssEL, OUTPUT);
   pinMode(cwPin, OUTPUT);
   pinMode(ccwPin, OUTPUT);
-
 }
 
 void loop() {
@@ -21,32 +22,40 @@ void loop() {
     // 文字列を整数に変換
     long degPulse = degPulseStr.toInt();
 
-    //  
+    //
     if (degPulse > 0) {
-      
-      int targetPin = 0; 
+
+      int targetPin = 0;
       switch (inputData.charAt(0)) {
-        case 'c':
+        case 'a':
+          digitalWrite(ssEL, LOW);
           targetPin = cwPin;
           break;
-        case 'w':
+        case 'b':
+          digitalWrite(ssEL, LOW);
+          targetPin = ccwPin;
+          break;
+        case 'c':
+          digitalWrite(ssEL, HIGH);
+          targetPin = cwPin;
+          break;
+        case 'd':
+          digitalWrite(ssEL, HIGH);
           targetPin = ccwPin;
           break;
       }
-      
+
       for (int pulse = 0; pulse < degPulse; pulse++) {
         digitalWrite(targetPin, HIGH);
         delayMicroseconds(delayMicroSec);
         digitalWrite(targetPin, LOW);
         delayMicroseconds(delayMicroSec);
       }
-
     }
-
+    digitalWrite(ssEL, LOW);
     digitalWrite(cwPin, LOW);
     digitalWrite(ccwPin, LOW);
-    
-    Serial.println("stop");
 
+    Serial.println("Stop");
   }
 }
